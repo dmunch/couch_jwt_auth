e66edf307a4e35429473477b052d2f9efc634ee7
Martynas <martynas@firmfirm.co>
2017-07-11 13:00:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 rebar get-deps co eunit
 # fix some incompatibility with couchdb plugins and rebar deps
-ls deps/*/ebin
-ls deps/*/priv
 for file in deps/*/ebin/*; do cp "$file" "ebin/$(basename $file)";done
-for file in deps/*/priv/*.so; do cp "$file" "priv/$(basename $file)";done
+# for file in deps/*/priv/*.so; do cp "$file" "priv/$(basename $file)";done
